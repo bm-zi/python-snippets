7b655e4f1cc146ed9ee882f9fed6df5567f55f56
bm-zi <33130624+bm-zi@users.noreply.github.com>
2019-04-01 10:24:09 -0400

    Update runme.py

--- runme.py
+++ runme.py
@@ -1,15 +1,21 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 # Author: bm-zi
 
 import os
 import codeExamples
-
+import time
+import sys
 
 '''
 use the following command to get a list of indexed functions:
 
 cat searchExamples.py | grep -E 'f[1-9]|f[1-9][0-9]' -A 1 | grep -v def | grep -v ^-- | while read line ; do echo "$i - $line"; let "i=i+1"; done | sed -n 's/\#//p'
 '''
+
+def restart():
+	print('Restarting ... ')
+	time.sleep(1)
+	os.execl(sys.executable, sys.executable, * sys.argv)
 
 
 index='''
@@ -45,27 +51,53 @@ index='''
 30 -  Exception Handling
 31 -  Out Of Function Exception Handling
 32 -  Guessing Game
+33 -  Lists : examples
+34 -  Lists : list of lists
+35 -  Lists : negative indexes
+36 -  Lists : getting sublists with slices
+37 -  Lists : getting a list’s length with len()
+38 -  Lists : changing values in a list with indexes
+39 -  Lists: list concatenation and list replication
+40 -  Help : How to use this program
 '''
 
 while True:
 	os.system('clear')
-	print(index)
+	print(index)  		# Display a list of codes sample names 
+
 	print('================================')
-	print('Enter a number to view the item:', end='')
-	num = int(input())
-	varstring1 = 'f' + str(num)
-	varstring2 = 'f' + str(num + 1)
-	print()
-	print('-------------------- CODE EXAMPLE --------------------')
-	command = 'cat codeExamples.py | sed -n \'/def\ ' + varstring1 + '()/,/def\ ' + varstring2 + '/p\' | grep -v '  + varstring1 + '| grep -v ' + varstring2
-	os.system(command)
+	i = input("Enter a number to view the item: ")
 
-	print('-------------------- CODE OUTPUT --------------------')
-	fct=getattr(codeExamples,varstring1)
-	fct()
+	try:
+		if str(i) == 'exit':      # type exit to exit program
+			sys.exit()
 
-	print()
-	print('==========================')
-	print('Press any key to continue!', end='')
-	input()
-	
+		if str(i) == 'r':
+			restart()
+
+		if not i:				   # if no value entered, restart the while loop
+			continue
+
+		num=int(i)
+		varstring1 = 'f' + str(num)
+		varstring2 = 'f' + str(num + 1)
+
+		os.system('clear')
+		print('-------------------- CODE EXAMPLE No ' + str(num) + ' --------------------')
+		command = 'cat codeExamples.py | sed -n \'/def\ ' + varstring1 + '()/,/def\ ' + varstring2 + '/p\' | grep -v '  + varstring1 + '| grep -v ' + varstring2
+		os.system(command)
+		print('-------------------- CODE No ' + str(num) + ' OUTPUT --------------------')
+		fct=getattr(codeExamples,varstring1)
+		fct()
+		print()
+		print('==========================')
+		print('Press any key to continue!', end='')
+		input()
+		
+	except ValueError:  		  # Caught error when input is a string
+		print('Not an integer!')
+		time.sleep(1)
+
+	except AttributeError:		  # Caught error when input integer is bigger than list items numbers
+		continue
+		
